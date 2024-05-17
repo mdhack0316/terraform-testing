06637fddb4ebc95869b620c576c863abbb4acb33
mdhack0316 <39210514+mdhack0316@users.noreply.github.com>
2024-05-17 18:30:52 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -2,7 +2,9 @@ resource "google_compute_instance" "my_instance" {
   name         = "my-instance"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
-
+  labels = {
+    environment = "production"
+   }
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-10"
