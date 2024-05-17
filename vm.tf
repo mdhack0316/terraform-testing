e72f12e0a7b4691333097c9f0103d888ba06483e
mdhack0316 <39210514+mdhack0316@users.noreply.github.com>
2024-05-17 18:08:26 +0530

    Create vm.tf

---
+++ vm.tf
@@ -0,0 +1,18 @@
+resource "google_compute_instance" "my_instance" {
+  name         = "my-instance"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
